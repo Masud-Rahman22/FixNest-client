3bf093a28c9da0004d3eec1243a3c420215e0d7a
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-02-29 19:09:24 +0600

    completed repair services breakdown with react scroll trigger and react countup package

---
+++ src/components/count_section/CountSection.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
+
+const CountSection = () => {
+    const [counterState, setCounterState] = useState(false)
+    const gridContainerStyle = {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(4, 1fr)', // Creates 4 columns with equal width
+        gap: '20px',
+        padding: "25px 30px"
+    };
+    return (
+        <div style={{ padding: "100px 40px", color: "black" }}>
+            <h1 className="font-inter font-extrabold text-4xl text-center my-10">Repair<span className="text-blue-400">-</span>Services<span className="text-blue-400">-</span>Breakdown</h1>
+            <ScrollTrigger onEnter={() => setCounterState(true)} onExit={() => setCounterState(false)} >
+                <div style={gridContainerStyle} className="border-2 border-blue-900">
+                    <div style={{ textAlign: "center" }}>
+                        <h2 style={{ fontSize: "60px", fontWeight: "bold" }}>{
+                            counterState && <CountUp start={0} end={95} duration={3.40}></CountUp>
+                        }%</h2>
+                        <p style={{ fontSize: "25px" }} className="text-blue-400 font-bold font-inter">Screen Replacement</p>
+                    </div>
+                    <div style={{ textAlign: "center" }}>
+                        <h2 style={{ fontSize: "60px", fontWeight: "bold" }}>{
+                            counterState && <CountUp start={0} end={98} duration={3.40}></CountUp>
+                        }%</h2>
+                        <p style={{ fontSize: "25px" }} className="text-blue-400 font-bold font-inter">Battery Replacement</p>
+                    </div>
+                    <div style={{ textAlign: "center" }}>
+                        <h2 style={{ fontSize: "60px", fontWeight: "bold" }}>{
+                            counterState && <CountUp start={0} end={94} duration={3.40}></CountUp>
+                        }%</h2>
+                        <p style={{ fontSize: "25px" }} className="text-blue-400 font-bold font-inter">Component Repair</p>
+                    </div>
+                    <div style={{ textAlign: "center" }}>
+                        <h2 style={{ fontSize: "60px", fontWeight: "bold" }}>{
+                            counterState && <CountUp start={0} end={89} duration={3.40}></CountUp>
+                        }%</h2>
+                        <p style={{ fontSize: "25px" }} className="text-blue-400 font-bold font-inter">Software Troubleshooting</p>
+                    </div>
+                </div>
+            </ScrollTrigger>
+        </div >
+    );
+};
+
+export default CountSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../components/Banner/Banner";
 import AboutUs from "../components/about_us/AboutUs";
 import ContactUs from "../components/contact_us/ContactUs";
+import CountSection from "../components/count_section/CountSection";
 import What_We_Do from "../components/what_we_do/What_We_Do";
 
 
@@ -11,6 +12,7 @@ const Home = () => {
             <What_We_Do></What_We_Do>
             <AboutUs></AboutUs>
             <ContactUs></ContactUs>
+            <CountSection></CountSection>
         </div>
     );
 };
